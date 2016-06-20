c6ad34db2c74535d098341777d5741cb5ecbb5da
Malte Götz <maltegoetz@outlook.com>
2016-06-20 09:26:08 +0200

    added my demo app

---
+++ DWXapp/DWXapp/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace DWXapp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public void OnLoadClicked(object sender, EventArgs args)
+        {
+            //check connectivity
+            //check connection type
+            //ping host
+                //load commit message
+                //store commitmsg
+                //try to load cached commitmsg
+                //check if cached msg exists
+            
+        }
+    }
+}
